2b83535b09fb490327053636e99a9c935e8ec79c
Brenden Crowie <brendencrowie@gmail.com>
2022-08-16 21:55:20 -0400

    Delete secret.py
    Deleted

--- secret.py
+++
@@ -1,9 +0,0 @@
-import praw
-
-reddit = praw.Reddit(
-    client_id="ycBqTj-eOI4BXzVtf8CKOw",
-    client_secret="Bt2ipPhDPIpWP__UIQ9fvnJMojR2Zg",
-    password="By#KnnvQr2Gb5x9m$q",
-    user_agent="treddit-notifs:0.1 by /u/cr0wstuf",
-    username="cr0wstuf",
-)
